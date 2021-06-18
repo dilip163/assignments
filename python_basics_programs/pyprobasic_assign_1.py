f81e4c5ba9bc363e9c1387fa1a369ca6bd4ae2f5
dilip163 <dilippotnuru66@gmail.com>
2021-06-18 17:30:08 +0530

    uploaded files

---
+++ python_basics_programs/pyprobasic_assign_1.py
@@ -0,0 +1,22 @@
+#1ans]
+print('Hello world')
+print('-'*50)
+#2ans]
+a,b=108,9
+print(a+b)
+print(a/b)
+print('-'*50)
+#3ans]
+height,base=10,5
+print('area of triangle:',(height*base)/2)
+print('-'*50)
+#4ans]
+a=10
+b=20
+a,b=b,a
+print(a,b)
+print('-'*50)
+#5ans]
+import random
+n = random.randint(0,22)
+print(n)
